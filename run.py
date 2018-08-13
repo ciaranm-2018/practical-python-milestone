93dc5f14ae624175f1b301ef3db5c54d37012a96
Ciaran Mitchell <ciaranmitchell88@gmail.com>
2018-08-13 21:54:15 +0000

    form labels and content

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 import os
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = 'some_secret'
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        flash("Thanks {}, enjoy the riddles!".format(request.form["name"]))
     return render_template("index.html")
 
 
@@ -21,7 +24,7 @@ def about():
 @app.route('/contact', methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print(request.form)
+        flash("Thanks {}, we have received your message!".format(request.form["name"]))
     return render_template("contact.html", page_heading="Contact")
 
 
